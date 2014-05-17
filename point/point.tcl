54d7fa6988a98cb9eed466da6155533e3dca7f0e
Alexey Ignatov <lexszero@gmail.com>
2014-05-18 01:26:25 +0400

    psto → point

--- point/point.tcl
+++ point/point.tcl
@@ -1,23 +1,23 @@
 package require msgcat
 
-option add *psto.nick			red		widgetDefault
-option add *psto.tag 			ForestGreen	widgetDefault
-option add *psto.my	    		gray		widgetDefault
-option add *psto.number  		blue		widgetDefault
-option add *psto.private_foreground	blue		widgetDefault
-option add *psto.private_background	#FF9A15		widgetDefault
+option add *point.nick			red		widgetDefault
+option add *point.tag 			ForestGreen	widgetDefault
+option add *point.my	    		gray		widgetDefault
+option add *point.number  		blue		widgetDefault
+option add *point.private_foreground	blue		widgetDefault
+option add *point.private_background	#FF9A15		widgetDefault
 
-namespace eval psto {
+namespace eval point {
 variable options
 variable chat_things
 
 ::msgcat::mcload [file join [file dirname [info script]] msgs]
 
-if {![::plugins::is_registered psto]} {
-    ::plugins::register psto \
+if {![::plugins::is_registered point]} {
+    ::plugins::register point \
               -namespace [namespace current] \
               -source [info script] \
-              -description [::msgcat::mc "Whether the psto plugin is loaded."] \
+              -description [::msgcat::mc "Whether the point plugin is loaded."] \
               -loadcommand [namespace code load] \
               -unloadcommand [namespace code unload]
     return
@@ -25,99 +25,99 @@ if {![::plugins::is_registered psto]} {
 
         custom::defgroup Plugins [::msgcat::mc "Plugins options."] -group Tkabber
 
-        set group "Psto"
+        set group "point"
         custom::defgroup $group \
-                [::msgcat::mc "Psto settings."] \
+                [::msgcat::mc "point settings."] \
                 -group Plugins
 
-        custom::defvar options(main_jid) "psto@psto.net/Psto" \
-                [::msgcat::mc "Main Psto JID. This used for forwarding things from other chats."] \
+        custom::defvar options(main_jid) "p@point.im/point" \
+                [::msgcat::mc "Main point JID. This used for forwarding things from other chats."] \
                 -group $group \
                 -type string
         custom::defvar options(nick) "" \
-                [::msgcat::mc "Your Psto nickame."] \
+                [::msgcat::mc "Your point nickame."] \
                 -group $group \
                 -type string
-        custom::defvar options(special_update_psto_tab) 1 \
+        custom::defvar options(special_update_point_tab) 1 \
                 [::msgcat::mc "Only private messages and replies to your comments is personal message."] \
                 -group $group \
                 -type boolean
 
 proc load {} {
-    ::richtext::entity_state psto_numbers 1
-    ::richtext::entity_state psto 1
-    ::richtext::entity_state psto_ligth 1
+    ::richtext::entity_state point_numbers 1
+    ::richtext::entity_state point 1
+    ::richtext::entity_state point_ligth 1
 
     hook::add draw_message_hook        [namespace current]::ignore_server_messages 0
     hook::add draw_message_hook        [namespace current]::handle_message 21
     hook::add chat_window_click_hook   [namespace current]::insert_from_window
-    hook::add rewrite_message_hook     [namespace current]::rewrite_psto_message 20
-    hook::add chat_send_message_hook   [namespace current]::rewrite_send_psto_message 19
+    hook::add rewrite_message_hook     [namespace current]::rewrite_point_message 20
+    hook::add chat_send_message_hook   [namespace current]::rewrite_send_point_message 19
 
-    hook::add draw_message_hook [namespace current]::update_psto_tab 8
+    hook::add draw_message_hook [namespace current]::update_point_tab 8
     hook::remove draw_message_hook ::plugins::update_tab::update 8
 
-    hook::add draw_message_hook [namespace current]::add_number_of_messages_from_psto_to_title 18
+    hook::add draw_message_hook [namespace current]::add_number_of_messages_from_point_to_title 18
     hook::remove draw_message_hook ::::ifacetk::add_number_of_messages_to_title 18
 
-    hook::add generate_completions_hook [namespace current]::psto_commands_comps 99
+    hook::add generate_completions_hook [namespace current]::point_commands_comps 99
 }
 
 proc unload {} {
     hook::remove draw_message_hook        [namespace current]::ignore_server_messages 0
     hook::remove draw_message_hook        [namespace current]::handle_message 21
     hook::remove chat_window_click_hook   [namespace current]::insert_from_window
-    hook::remove rewrite_message_hook     [namespace current]::rewrite_psto_message 20
-    hook::remove chat_send_message_hook   [namespace current]::rewrite_send_psto_message 19
+    hook::remove rewrite_message_hook     [namespace current]::rewrite_point_message 20
+    hook::remove chat_send_message_hook   [namespace current]::rewrite_send_point_message 19
 
-    hook::remove draw_message_hook [namespace current]::update_psto_tab 8
+    hook::remove draw_message_hook [namespace current]::update_point_tab 8
     hook::add draw_message_hook ::plugins::update_tab::update 8
 
-    hook::remove draw_message_hook [namespace current]::add_number_of_messages_from_psto_to_title 18
+    hook::remove draw_message_hook [namespace current]::add_number_of_messages_from_point_to_title 18
     hook::add draw_message_hook ::::ifacetk::add_number_of_messages_to_title 18
 
-    hook::remove generate_completions_hook [namespace current]::psto_commands_comps 99
+    hook::remove generate_completions_hook [namespace current]::point_commands_comps 99
 
-    ::richtext::entity_state psto_numbers 0
-    ::richtext::entity_state psto 0
-    ::richtext::entity_state psto_ligth 0
+    ::richtext::entity_state point_numbers 0
+    ::richtext::entity_state point 0
+    ::richtext::entity_state point_ligth 0
 }
 
-proc is_psto_jid {jid} {
+proc is_point_jid {jid} {
     set jid [::xmpp::jid::removeResource $jid]
     set node [::xmpp::jid::node $jid]
-    return [expr [cequal $jid "psto@psto.net"]]
+    return [expr [cequal $jid "p@point.im"]]
 }
 
-proc is_psto {chatid} {
+proc is_point {chatid} {
     set jid [chat::get_jid $chatid]
-    return [is_psto_jid $jid]
+    return [is_point_jid $jid]
 }
 
 proc handle_message {chatid from type body x} {
-    if {![is_psto $chatid]} return
+    if {![is_point $chatid]} return
 
-    ::richtext::property_add {PSTO} {}
+    ::richtext::property_add {point} {}
 
     set chatw [chat::chat_win $chatid]
     set jid [chat::get_jid $chatid]
 
     set tags {}
     if {![cequal $jid $from]} {
-        lappend tags PSTOMY
+        lappend tags pointMY
     }
 
     ::richtext::render_message $chatw $body $tags
     return stop
 }
 
-proc is_personal_psto_message {from body} {
+proc is_personal_point_message {from body} {
     variable options
 
     set reply_to_my_comment 0
 
     set private_msg [regexp {^P @.+ -> @.+:\n} $body]
-    set reply_to_comment [regexp {\n(@.+|@.+ -> @.+):\n>.+\n\n.+\n\n#[a-z]+/\d+ \(\d+\) http://psto.net/[a-z]+#\d+$} $body -> reply_to_nick]
+    set reply_to_comment [regexp {\n(@.+|@.+ -> @.+):\n>.+\n\n.+\n\n#[a-z]+/\d+ \(\d+\) http://point.im/[a-z]+#\d+$} $body -> reply_to_nick]
 
     if {$reply_to_comment} {
         set reply_to_my_comment [cequal $options(nick) $reply_to_nick]
@@ -126,9 +126,9 @@ proc is_personal_psto_message {from body} {
     return [expr $private_msg || $reply_to_my_comment]
 }
 
-proc update_psto_tab {chatid from type body x} {
+proc update_point_tab {chatid from type body x} {
     variable options
-    if {![expr [is_psto_jid $from] && [cequal $type "chat"] && $options(special_update_psto_tab)]} {
+    if {![expr [is_point_jid $from] && [cequal $type "chat"] && $options(special_update_point_tab)]} {
         ::plugins::update_tab::update $chatid $from $type $body $x
         return
     }
@@ -143,7 +143,7 @@ proc update_psto_tab {chatid from type body x} {
 
     set cw [chat::winid $chatid]
 
-    if {[is_personal_psto_message $from $body]} {
+    if {[is_personal_point_message $from $body]} {
         tab_set_updated $cw 1 mesg_to_user
     } else {
         tab_set_updated $cw 1 message
@@ -151,14 +151,14 @@ proc update_psto_tab {chatid from type body x} {
 }
 
 proc ignore_server_messages {chatid from type body x} {
-    if {[is_psto $chatid] && $from == ""} {
+    if {[is_point $chatid] && $from == ""} {
         return stop;
     }
 }
 
-proc add_number_of_messages_from_psto_to_title {chatid from type body x} {
+proc add_number_of_messages_from_point_to_title {chatid from type body x} {
     variable options
-    if {![expr [is_psto_jid $from] && [cequal $type "chat"] && $options(special_update_psto_tab)]} {
+    if {![expr [is_point_jid $from] && [cequal $type "chat"] && $options(special_update_point_tab)]} {
         ::ifacetk::add_number_of_messages_to_title $chatid $from $type $body $x
         return
     }
@@ -179,7 +179,7 @@ proc add_number_of_messages_from_psto_to_title {chatid from type body x} {
 
     incr number_msg($chatid)
 
-    if {[is_personal_psto_message $from $body]} {
+    if {[is_personal_point_message $from $body]} {
         incr personal_msg($chatid)
     }
 
@@ -187,7 +187,7 @@ proc add_number_of_messages_from_psto_to_title {chatid from type body x} {
     ::ifacetk::update_main_window_title
 }
 
-proc rewrite_psto_message \
+proc rewrite_point_message \
      {vxlib vfrom vid vtype vis_subject vsubject \
       vbody verr vthread vpriority vx} {
     upvar 2 $vfrom from
@@ -195,7 +195,7 @@ proc rewrite_psto_message \
     upvar 2 $vbody body
     upvar 2 $vx x
 
-    if {![is_psto_jid $from] || ![cequal $type "chat"]} {
+    if {![is_point_jid $from] || ![cequal $type "chat"]} {
         return
     }
 
@@ -214,8 +214,8 @@ proc rewrite_psto_message \
     set x $newx
 }
 
-proc rewrite_send_psto_message {chatid user body type} {
-    if {![is_psto $chatid] || ![cequal $type "chat"]} {
+proc rewrite_send_point_message {chatid user body type} {
+    if {![is_point $chatid] || ![cequal $type "chat"]} {
         return
     }
 
@@ -241,13 +241,13 @@ proc insert_from_window {chatid w x y} {
 
     set tags [$cw tag names "@$x,$y"]
 
-    if {[set idx [lsearch -glob $tags PSTO-*]] >= 0} {
-        set thing [string range [lindex $tags $idx] 5 end]
+    if {[set idx [lsearch -glob $tags point-*]] >= 0} {
+        set thing [string range [lindex $tags $idx] 6 end]
     }
 
     if {$thing == ""} return
 
-    if {![is_psto_jid $jid]} {
+    if {![is_point_jid $jid]} {
         set xlib [chat::get_xlib $chatid]
         set mainchat [chat::chatid $xlib $options(main_jid)]
 
@@ -265,8 +265,8 @@ proc insert_from_window {chatid w x y} {
     return stop
 }
 
-proc psto_commands_comps {chatid compsvar wordstart line} {
-    if {![is_psto $chatid]} return
+proc point_commands_comps {chatid compsvar wordstart line} {
+    if {![is_point $chatid]} return
 
     upvar 0 $compsvar comps
     variable chat_things
@@ -304,31 +304,31 @@ proc correct_command {chatid user body type} {
 # --------------
 # RichText stuff
 
-proc configure_psto {w} {
-    set options(psto.nick) [option get $w psto.nick Text]
-    set options(psto.tag) [option get $w psto.tag Text]
-    set options(psto.my) [option get $w psto.my Text]
+proc configure_point {w} {
+    set options(point.nick) [option get $w point.nick Text]
+    set options(point.tag) [option get $w point.tag Text]
+    set options(point.my) [option get $w point.my Text]
 
-    $w tag configure PSTONICK -foreground $options(psto.nick)
-    $w tag configure PSTOTAG -foreground $options(psto.tag)
-    $w tag configure PSTOMY -foreground $options(psto.my)
+    $w tag configure pointNICK -foreground $options(point.nick)
+    $w tag configure pointTAG -foreground $options(point.tag)
+    $w tag configure pointMY -foreground $options(point.my)
 }
 
-proc configure_psto_numbers {w} {
-    set options(psto.number) [option get $w psto.number Text]
+proc configure_point_numbers {w} {
+    set options(point.number) [option get $w point.number Text]
 
-    $w tag configure PSTONUM -foreground $options(psto.number)
+    $w tag configure pointNUM -foreground $options(point.number)
 }
 
-proc configure_psto_ligth {w} {
-    set options(psto.private_foreground) [option get $w psto.private_foreground Text]
-    set options(psto.private_background) [option get $w psto.private_background Text]
+proc configure_point_ligth {w} {
+    set options(point.private_foreground) [option get $w point.private_foreground Text]
+    set options(point.private_background) [option get $w point.private_background Text]
 
-    $w tag configure PSTOLIGTH -foreground $options(psto.private_foreground)
-    $w tag configure PSTOLIGTH -background $options(psto.private_background)
+    $w tag configure pointLIGTH -foreground $options(point.private_foreground)
+    $w tag configure pointLIGTH -background $options(point.private_background)
 }
 
-proc spot_psto_ligth {what at startVar endVar} {
+proc spot_point_ligth {what at startVar endVar} {
     set matched [regexp -indices -start $at -- \
     {(^PM)(?: from @.+:\n)} $what -> bounds]
 
@@ -339,7 +339,7 @@ proc spot_psto_ligth {what at startVar endVar} {
     return true
 }
 
-proc spot_psto {what at startVar endVar} {
+proc spot_point {what at startVar endVar} {
     # WTF IS THIS?
     set matched [regexp -indices -start $at -- \
     {(?:\s|\n|\A|\(|\>)(@[\w@.-]+|\*[\w?!+'/.-]+)(?:(\.(\s|\n))?)} $what -> bounds]
@@ -351,7 +351,7 @@ proc spot_psto {what at startVar endVar} {
     return true
 }
 
-proc spot_psto_numbers {what at startVar endVar} {
+proc spot_point_numbers {what at startVar endVar} {
     set matched [regexp -indices -start $at -- \
     {(?:\s|\n|\A|\(|\>)(#[a-z]+(/\d+)?)(?:(\.(\s|\n))?)} $what -> bounds]
     if {!$matched} { return false }
@@ -361,19 +361,19 @@ proc spot_psto_numbers {what at startVar endVar} {
     return true
 }
 
-proc process_psto {atLevel accName} {
-    if {[::richtext::property_exists {PSTO}]} {
-        return [process $atLevel $accName psto]
+proc process_point {atLevel accName} {
+    if {[::richtext::property_exists {point}]} {
+        return [process $atLevel $accName point]
     }
 }
 
-proc process_psto_numbers {atLevel accName} {
-    return [process $atLevel $accName psto_numbers]
+proc process_point_numbers {atLevel accName} {
+    return [process $atLevel $accName point_numbers]
 }
 
-proc process_psto_ligth {atLevel accName} {
-    if {[::richtext::property_exists {PSTO}]} {
-       return [process $atLevel $accName psto_ligth]
+proc process_point_ligth {atLevel accName} {
+    if {[::richtext::property_exists {point}]} {
+       return [process $atLevel $accName point_ligth]
     }
 }
 
@@ -383,14 +383,14 @@ proc process {atLevel accName what} {
     set out {}
 
     foreach {s type tags} $chunks {
-        if {[lsearch -regexp $type (text)|(psto_ligth)]<0} {
+        if {[lsearch -regexp $type (text)|(point_ligth)]<0} {
             # pass through
             lappend out $s $type $tags
             continue
         }
 
-        if {[expr [lsearch -exact $type psto_ligth]>=0]} {
-        lappend tags PSTOLIGTH
+        if {[expr [lsearch -exact $type point_ligth]>=0]} {
+        lappend tags pointLIGTH
         }
 
         set index 0; set uStart 0; set uEnd 0
@@ -413,22 +413,22 @@ proc process {atLevel accName what} {
     set chunks $out
 }
 
-proc render_psto {w type thing tags args} {
-    if {[expr [lsearch -exact $tags PSTOLIGTH]<0]} {
+proc render_point {w type thing tags args} {
+    if {[expr [lsearch -exact $tags pointLIGTH]<0]} {
        if {[cequal [string index $thing 0] "#" ]} {
-          set type PSTONUM
+          set type pointNUM
           } else {
                  if {[cequal [string index $thing 0] "*" ]} {
-                    set type PSTOTAG
+                    set type pointTAG
                     } else {
                            if {[cequal [string index $thing 0] "@" ]} {
-                               set type PSTONICK
+                               set type pointNICK
                                }
                            }
                  }
     } else {
-           if {[lsearch -exact $tags PSTOLIGTH]>=0} {
-               set type PSTOLIGTH
+           if {[lsearch -exact $tags pointLIGTH]>=0} {
+               set type pointLIGTH
            }
     }
 
@@ -443,33 +443,33 @@ proc render_psto {w type thing tags args} {
             }
 #################
 
-    set id PSTO-$thing
-    $w insert end $thing [lfuse $tags [list $id $type PSTO]]
+    set id point-$thing
+    $w insert end $thing [lfuse $tags [list $id $type point]]
     return $id
 }
 
-proc render_psto_ligth {w type thing tags args} {
-    set id PSTOLIGTH-$thing
-    $w insert end $thing [lfuse $tags [list $id $type PSTOLIGTH]]
+proc render_point_ligth {w type thing tags args} {
+    set id pointLIGTH-$thing
+    $w insert end $thing [lfuse $tags [list $id $type pointLIGTH]]
     return $id
 }
 
-::richtext::register_entity psto_numbers \
-    -configurator [namespace current]::configure_psto_numbers \
-    -parser [namespace current]::process_psto_numbers \
-    -renderer [namespace current]::render_psto \
+::richtext::register_entity point_numbers \
+    -configurator [namespace current]::configure_point_numbers \
+    -parser [namespace current]::process_point_numbers \
+    -renderer [namespace current]::render_point \
     -parser-priority 53
 
-::richtext::register_entity psto_ligth \
-    -configurator [namespace current]::configure_psto_ligth \
-    -parser [namespace current]::process_psto_ligth \
-    -renderer [namespace current]::render_psto_ligth \
+::richtext::register_entity point_ligth \
+    -configurator [namespace current]::configure_point_ligth \
+    -parser [namespace current]::process_point_ligth \
+    -renderer [namespace current]::render_point_ligth \
     -parser-priority 81
 
-::richtext::register_entity psto \
-    -configurator [namespace current]::configure_psto \
-    -parser [namespace current]::process_psto \
-    -renderer [namespace current]::render_psto \
+::richtext::register_entity point \
+    -configurator [namespace current]::configure_point \
+    -parser [namespace current]::process_point \
+    -renderer [namespace current]::render_point \
     -parser-priority 85
 }
 # vi:ts=4:et
